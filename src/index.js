28f71ab8207751b5e4327f9b749a8f5981a47c48
Natalia2022r <99670282+Natalia2022r@users.noreply.github.com>
2022-08-11 22:01:00 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 import store from "./redux/store";
 
@@ -11,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-        <HashRouter>
+
             <App />
-            </HashRouter>
+     
         </Provider>
     </React.StrictMode>
 );
