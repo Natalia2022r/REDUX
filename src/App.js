de18f37d30e9cdfca8fb4582c4d460736e3cdccb
Natalia2022r <99670282+Natalia2022r@users.noreply.github.com>
2022-08-11 21:48:15 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import PostList from "./Posts/PostList";
 import AddPostForm from "./Posts/AddPostForm"
 
 function App() {
     return (
         <div className="App">
+        <HashRouter>
         <Routes>
         <Route exact path="/" element ={<PostList />}/>
         <Route exact path="/addform" element= {<AddPostForm/>}/>
          
             </Routes>
+            </HashRouter>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import store from "./redux/store";
 
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
